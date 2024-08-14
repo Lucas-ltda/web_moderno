5dacf26b3d2d0776b2080936b6ba3f037a9bae24
Lucas <lucaslksleite@gmail.com>
2024-08-13 22:09:56 -0300

    Algumas alterações no ecma script

---
+++ JavaScript/Objeto/notacoes.js
@@ -0,0 +1,31 @@
+const a = 1
+const b = 2
+const c = 3
+
+const obj1 = {a: a, b: b , c: c}
+console.log(obj1)
+
+const obj2 = {a, b, c}
+console.log(obj2)
+
+
+const nomeDoAtributo = "Nota Da Pessoa"
+const valorDoAtributo = 8.75
+
+const obj3 = {}
+
+obj3[nomeDoAtributo] = valorDoAtributo
+console.log(obj3)
+
+const obj4 = {[nomeDoAtributo]:valorDoAtributo}
+
+console.log(obj4)
+
+const obj5 = {
+    funcao1: function (){
+        //...
+    },
+    function(){
+        //...
+    }
+}
